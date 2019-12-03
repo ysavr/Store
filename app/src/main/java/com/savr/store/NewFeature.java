95564a41e403f2496d15eef79485607e8459b925
Yusuf Safrudin <y.safrudin666@gmail.com>
2019-12-03 10:21:34 +0700

    add construtor

--- app/src/main/java/com/savr/store/NewFeature.java
+++ app/src/main/java/com/savr/store/NewFeature.java
@@ -1,7 +1,39 @@
 package com.savr.store;
 
 public class NewFeature {
-    String name;
-    String address;
-    int age;
+    private String name;
+    private String address;
+    private int age;
+
+    public NewFeature(String name, String address, int age) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
+
+
